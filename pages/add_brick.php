a5a874302658006348a2671c22fde6d12ad21586
Zach Terault <teraultz@gmail.com>
2026-03-27 10:41:49 -0400

    Add Brick Functionality
    chnaged file ending to php for admin_home link to add_brick.php and tried getting add brick to work correctly

---
+++ pages/add_brick.php
@@ -0,0 +1,86 @@
+<?php
+    if (isset($_POST['send'])){
+        $errors = array();
+        $success = "";
+
+        $column= filter_var(trim($_POST['column']), FILTER_SANITIZE_STRING);
+        if (empty($column)){
+		    $errors['column'] = 'A column is required:';
+        }
+        $row= filter_var(trim($_POST['row']), FILTER_SANITIZE_STRING);
+        if (empty($row)){
+		    $errors['row'] = 'An row is required:';
+        }
+        $location= filter_var(trim($_POST['location']), FILTER_SANITIZE_STRING);
+        if (empty($location)){
+		    $errors['location'] = 'An location is required:';
+        }
+        $name= filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
+        if (empty($name)){
+		    $errors['name'] = 'An name is required:';
+        }
+        $search = substr($name, 0, 3);
+        $gridReference = $column . $row;
+
+        if (!$errors){
+            require_once ('../pdo_connect.php');
+            $sql = "INSERT INTO Bricks (Search, Name, ColumnVal, RowVal, GridReference, Location) VALUES (?, ?, ?, ?, ?, ?)";
+            $stmt = $dbc->prepare($sql);
+            $stmt->bindParam(1, $search);
+            $stmt->bindParam(2, $name);
+            $stmt->bindParam(3, $column);
+            $stmt->bindParam(4, $row);
+            $stmt->bindParam(5, $gridReference);
+            $stmt->bindParam(6, $location);
+            $stmt->execute();
+            $success = "Brick entry was added successfully";
+        }
+    }
+?>
+
+<head>
+	<meta charset="utf-8">
+	<title>SB Vets MMS</title>
+	<link rel="stylesheet" href="../styles/main.css">
+</head>
+
+<body>
+	<div class="header">
+		<h1><a href="../index.html">SB Vets Memorial Management System</a></h1>
+	</div>
+	
+	<h2>Add Brick Entry</h2>
+	
+	<div class="add">
+        <form method="post" action="add_brick.php">
+            <?php
+                if ($errors['column']) echo "<h2 class=\"warning\">{$errors['column']}</h2>"; 
+                if ($errors['row']) echo "<h2 class=\"warning\">{$errors['row']}</h2>"; 
+                if ($errors['location']) echo "<h2 class=\"warning\">{$errors['location']}</h2>";
+                if ($errors['name']) echo "<h2 class=\"warning\">{$errors['name']}</h2>"; 
+                echo $success; 
+            ?>
+            <div class="addBrick">
+                <h3 class="updateHead">Brick Location:</h3>
+                
+                <input name="column" class="input" placeholder="Column">
+                
+                <input name="row" class="input" placeholder="Row">
+                
+                <input name="location" class="input" placeholder="Location">
+                
+            </div>
+            
+            <div class="addBrick">
+                <h3 class="updateHead">Name for Brick:</h3>
+                
+                <input name="name" class="input" placeholder="Name:">
+                
+                <br><br>
+                            
+                <button name="send" type="submit" class="update-btn">Add Entry</button>
+            </div>
+        </form>
+	</div>
+</body>
+</html>
