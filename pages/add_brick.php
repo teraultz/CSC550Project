7209477fa757437cd8e9a1064671819acb2be9ff
anna-knowles <annaknowles1253@gmail.com>
2026-04-22 13:49:37 -0400

    Update add_brick.php with return button

--- pages/add_brick.php
+++ pages/add_brick.php
@@ -81,6 +81,11 @@
         ?>
         <h4><a href="session_logout.php">Logout</a></h4>
 	</div>
+
+	<!---Added Return button--->
+	<div class="back">
+		<button onclick="history.back()">← Return to previous page</button>
+	</div>
 	
 	<h2>Add Brick Entry</h2>
 	
